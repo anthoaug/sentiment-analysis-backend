c8c157fb6b69c7667a048fd5846adbfa810777e1
Anthony Augustin <anthoaug@gmail.com>
2022-11-06 20:51:03 -0500

    Implement complement naive Bayes

--- util/__init__.py
+++ util/__init__.py
@@ -1,4 +1,20 @@
 from util.youtube import CommentData, get_youtube_comments
-from util.model import SentimentModel, model_instance, predict_sentiment
+from util.model import MultinomialBayes, ComplementBayes
 
-__all__ = [SentimentModel, CommentData, model_instance, get_youtube_comments, predict_sentiment]
+
+def _init_model():
+    import json
+
+    with open("../static/model/model.json") as model_file:
+        model_config = json.load(model_file)
+        return MultinomialBayes(**model_config)
+
+
+model_instance: MultinomialBayes = _init_model()
+
+
+def predict_sentiment(text: str) -> str:
+    return model_instance.predict(text)
+
+
+__all__ = [MultinomialBayes, CommentData, model_instance, get_youtube_comments, predict_sentiment]
--- util/model.py
+++ util/model.py
@@ -1,4 +1,5 @@
-import django.contrib.staticfiles.storage
+from abc import abstractmethod, ABC
+
 import collections
 import pandas
 import numpy
@@ -7,7 +8,10 @@ import nltk
 import sys
 
 
-class SentimentModel:
+class NaiveBayesModel(ABC):
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    lemmatizer = nltk.WordNetLemmatizer()
+
     def __init__(self, features: dict[str, dict[str, float]] = None, prior_positive: float = 0,
                  prior_negative: float = 0, prior_neutral: float = 0, smooth_positive: float = 0,
                  smooth_negative: float = 0, smooth_neutral: float = 0) -> None:
@@ -24,40 +28,36 @@ class SentimentModel:
         self.smooth_negative: float = smooth_negative
         self.smooth_neutral: float = smooth_neutral
 
-        self.stop_words = set(nltk.corpus.stopwords.words('english'))
-        self.lemmatizer = nltk.WordNetLemmatizer()
-
-    def tokenize(self, text: str) -> list[str]:
+    @staticmethod
+    def tokenize(text: str) -> list[str]:
         return [
-            self.lemmatizer.lemmatize(word)
+            NaiveBayesModel.lemmatizer.lemmatize(word)
             for word
             in nltk.word_tokenize(text.lower())
-            if word not in self.stop_words
+            if word not in NaiveBayesModel.stop_words
         ]
 
-    def train(self, data: pandas.DataFrame, text_label="text", sentiment_label="sentiment", classifier=None) -> None:
-        def default_classifier(sentiment_id, positive, negative, neutral):
-            if sentiment_id == 0:
-                return neutral
-            elif sentiment_id == 1:
-                return negative
-            elif sentiment_id == 2:
-                return positive
+    @staticmethod
+    def default_classifier(sentiment_id, positive, negative, neutral):
+        if sentiment_id == 0:
+            return neutral
+        elif sentiment_id == 1:
+            return negative
+        elif sentiment_id == 2:
+            return positive
 
-            return None
+        return None
 
-        if classifier is None:
-            classifier = default_classifier
-
+    @staticmethod
+    def count(data: pandas.DataFrame, text_label="text", sentiment_label="sentiment", classifier=default_classifier):
         counter_positive = collections.Counter()
         counter_negative = collections.Counter()
         counter_neutral = collections.Counter()
 
         text: str
         for index, text, sentiment in data[[text_label, sentiment_label]].itertuples():
-            words: list[str] = self.tokenize(text)
+            words: list[str] = NaiveBayesModel.tokenize(text)
             if len(words) == 0:
-                print(f"Invalid text \"{text}\" at index {index}.", file=sys.stderr)
                 continue
 
             counter = classifier(sentiment, counter_positive, counter_negative, counter_neutral)
@@ -68,6 +68,24 @@ class SentimentModel:
             for word in words:
                 counter[word] += 1
 
+        return counter_positive, counter_negative, counter_neutral
+
+    @abstractmethod
+    def train(self, data: pandas.DataFrame, text_label="text", sentiment_label="sentiment",
+              classifier=default_classifier) -> None:
+        pass
+
+    @abstractmethod
+    def predict(self, text: str) -> str:
+        pass
+
+
+class MultinomialBayes(NaiveBayesModel):
+    def train(self, data: pandas.DataFrame, text_label="text", sentiment_label="sentiment",
+              classifier=NaiveBayesModel.default_classifier) -> None:
+
+        counter_positive, counter_negative, counter_neutral = self.count(data, text_label, sentiment_label, classifier)
+
         total_positive = counter_positive.total()
         total_negative = counter_negative.total()
         total_neutral = counter_neutral.total()
@@ -77,15 +95,15 @@ class SentimentModel:
         self.prior_negative = math.log(total_negative / total_documents)
         self.prior_neutral = math.log(total_neutral / total_documents)
 
-        self.smooth_positive = -math.log(total_positive + total_documents)
+        self.smooth_positive = -math.log(total_documents + 3)
         for word, count in counter_positive.items():
             self.features["positive"][word] = math.log(count + 1) + self.smooth_positive
 
-        self.smooth_negative = -math.log(total_negative + total_documents)
+        self.smooth_negative = -math.log(total_documents + 3)
         for word, count in counter_negative.items():
             self.features["negative"][word] = math.log(count + 1) + self.smooth_negative
 
-        self.smooth_neutral = -math.log(total_neutral + total_documents)
+        self.smooth_neutral = -math.log(total_documents + 3)
         for word, count in counter_neutral.items():
             self.features["neutral"][word] = math.log(count + 1) + self.smooth_neutral
 
@@ -118,16 +136,60 @@ class SentimentModel:
         return ["positive", "negative", "neutral"][numpy.argmax([prob_positive, prob_negative, prob_neutral])]
 
 
-def _init_model():
-    import json
+# TODO: Fix. Accuracy is extremely low compared to multinomial, so likely there's a significant mistake impacting accuracy.
+class ComplementBayes(NaiveBayesModel):
+    def train(self, data: pandas.DataFrame, text_label="text", sentiment_label="sentiment",
+              classifier=NaiveBayesModel.default_classifier) -> None:
+        counter_positive, counter_negative, counter_neutral = self.count(data, text_label, sentiment_label, classifier)
 
-    with open("static/model/model.json") as model_file:
-        model_config = json.load(model_file)
-        return SentimentModel(**model_config)
+        total_positive = counter_positive.total()
+        total_negative = counter_negative.total()
+        total_neutral = counter_neutral.total()
+        total_documents = total_positive + total_negative + total_neutral
 
+        self.prior_positive = math.log(total_positive / total_documents)
+        self.prior_negative = math.log(total_negative / total_documents)
+        self.prior_neutral = math.log(total_neutral / total_documents)
 
-model_instance: SentimentModel = _init_model()
+        self.smooth_positive = math.log(total_negative + total_neutral + 3)
+        for word, count in counter_positive.items():
+            self.features["positive"][word] = self.smooth_positive - math.log(
+                counter_negative[word] + counter_neutral[word] + 1)
 
+        self.smooth_negative = math.log(total_positive + total_neutral + 3)
+        for word, count in counter_negative.items():
+            self.features["negative"][word] = self.smooth_negative - math.log(
+                counter_positive[word] + counter_neutral[word] + 1)
 
-def predict_sentiment(text: str) -> str:
-    return model_instance.predict(text)
+        self.smooth_neutral = math.log(total_positive + total_negative + 3)
+        for word, count in counter_neutral.items():
+            self.features["neutral"][word] = self.smooth_neutral - math.log(
+                counter_positive[word] + counter_negative[word] + 1)
+
+    def predict(self, text: str) -> str:
+        words = self.tokenize(text)
+
+        prob_positive = self.prior_positive
+        prob_negative = self.prior_negative
+        prob_neutral = self.prior_neutral
+        for word in words:
+            positive = word in self.features["positive"]
+            negative = word in self.features["negative"]
+            neutral = word in self.features["neutral"]
+
+            if positive:
+                prob_positive += self.features["positive"][word]
+            elif negative or neutral:
+                prob_positive += self.smooth_positive
+
+            if negative:
+                prob_negative += self.features["negative"][word]
+            elif positive or neutral:
+                prob_negative += self.smooth_negative
+
+            if neutral:
+                prob_neutral += self.features["neutral"][word]
+            elif positive or negative:
+                prob_neutral += self.smooth_neutral
+
+        return ["positive", "negative", "neutral"][numpy.argmin([prob_positive, prob_negative, prob_neutral])]
