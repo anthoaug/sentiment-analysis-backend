b5460716ac5756d5e89926d8405132b5586c6ce9
Anthony Augustin <anthoaug@gmail.com>
2022-11-27 23:27:55 -0500

    Allow all origins for testing

--- backend/settings.py
+++ backend/settings.py
@@ -127,10 +127,12 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000"
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_COOKIE_SAMESITE = 'Strict'
 SESSION_COOKIE_SAMESITE = 'Strict'
