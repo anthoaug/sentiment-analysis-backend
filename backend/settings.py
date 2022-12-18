36c93f6b7ba8dad2bfd5dc767fa8920d718971e5
Anthony Augustin <anthoaug@gmail.com>
2022-12-18 00:08:36 -0500

    Test

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-env.eba-yjk4y3nd.us-west-2.elasticbeanstalk.com"]
 
 # Application definition
 
